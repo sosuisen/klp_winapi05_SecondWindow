3241336ff610e8ee438e4c2a8c0c5ea98e8775ac
sosuisen <hidekazu.kubota@gmail.com>
2022-09-13 19:17:05 +0900

    first commit

---
+++ SecondWindow/SecondWindow.cpp
@@ -0,0 +1,57 @@
+﻿#include <windows.h>
+#include <string>
+
+LRESULT CALLBACK WndProc(
+    HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int APIENTRY wWinMain(HINSTANCE hInstance,
+    HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+    TCHAR szAppName[] = L"TestApp";
+    WNDCLASS wc;
+    HWND hwnd;
+    MSG msg;
+
+    // ウィンドウクラスの属性を設定
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = szAppName;
+
+    // ウィンドウクラスを登録
+    if (!RegisterClass(&wc)) return 0;
+    // ウィンドウを作成
+    hwnd = CreateWindow(
+        szAppName, L"Title",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL, NULL,
+        hInstance, NULL);
+    if (!hwnd) return 0;
+    // ウィンドウを表示
+    ShowWindow(hwnd, nCmdShow);
+    // ウィンドウを再描画
+    UpdateWindow(hwnd);
+    while (GetMessage(&msg, NULL, 0, 0) > 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
